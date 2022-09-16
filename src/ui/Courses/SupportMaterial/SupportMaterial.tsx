df29f2e02800244bd747213d0743afa5840a0398
Paulo Oliveira <paulo.oliveira@snowmanlabs.com>
2022-09-16 08:19:37 -0300

    fix: fixes on initial screen

---
+++ src/ui/Courses/SupportMaterial/SupportMaterial.tsx
@@ -0,0 +1,29 @@
+import supMaterialImg from "../../../assets/sup-material.png";
+import "./SupportMaterial.scss";
+
+const SupportMaterial = () => {
+  return (
+    <div className="support-material">
+      <div className="support-material__content">
+        <div className="support-material__content__img">
+          <img src={supMaterialImg} alt="Material de apoio" />
+        </div>
+        <div className="support-material__content__info">
+          <span className="title">Material de apoio</span>
+          <span className="description">
+            Além dos cursos e aulas, aqui você encontrará vídeos, manuais,
+            apresentações e muito conteúdo para enriquecer ainda mais seu
+            trabalho! Aproveite tudo que a Academia de Profissionais oferece
+            especialmente para você, profissional da construção!
+          </span>
+
+          <div className="support-material__content__info__buttons">
+            <button className="button">Baixar Material</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SupportMaterial;
--- src/ui/Home/Home.tsx
+++ src/ui/Home/Home.tsx
@@ -2,6 +2,8 @@ import "../../App.css";
 import CarouselBanner from "../Banners/CarouselBanner/CarouselBanner";
 import DetailsBanner from "../Banners/DetailsBanner/DetailsBanner";
 import CourseCategory from "../Courses/CourseCategory/CourseCategory";
+import FeaturedCourses from "../Courses/FeaturedCourses/FeaturedCourses";
+import SupportMaterial from "../Courses/SupportMaterial/SupportMaterial";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
@@ -10,6 +12,8 @@ const Home = () => {
     <div className="App">
       <Header></Header>
       <CarouselBanner></CarouselBanner>
+      <FeaturedCourses></FeaturedCourses>
+      <SupportMaterial></SupportMaterial>
       <CourseCategory></CourseCategory>
       <DetailsBanner></DetailsBanner>
       <Footer></Footer>
